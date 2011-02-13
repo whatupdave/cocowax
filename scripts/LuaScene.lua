658a066d52783dc0655f657d3445100b4ea6833b
Dave Newman <dave@snappyco.de>
2011-02-13 20:48:04 +1100

    Started chipmunk physics and a sample ball

--- scripts/LuaScene.lua
+++ scripts/LuaScene.lua
@@ -8,6 +8,25 @@ function init(self)
 	label:setPosition(CGPoint(size.width / 2, size.height / 2))
 	label:setColor(ccc3_from(color.Gold))
 	self:addChild(label)
-	
+
+	self:setupPhysics()
+	self:addBall()
 	return self
+end
+
+function setupPhysics(self)
+	local smgr = SpaceManagerCocos2d:init()
+	
+	smgr:addWindowContainmentWithFriction_elasticity_inset(1.0, 1.0, ccp(0,0))
+	smgr:setConstantDt(1/55)
+
+	smgr:start(0)
+	self.smgr = smgr
+end
+
+function addBall(self)
+	ball = self.smgr:addCircleAt_mass_radius(ccp(240,160), 1.0, 10)
+	ballSprite = cpCCSprite:spriteWithShape_file(ball,"ball.png")
+	self:addChild(ballSprite)
+	ballSprite:setIgnoreRotation(true)
 end
---
+++ scripts/chipmunk/init.lua
@@ -0,0 +1,1 @@
+require 'chipmunk/structs'
